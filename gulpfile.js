1db72f78e2fc6f23c22b9bbdcda198bfac83d564
Collin Cahill <collin.cahill@banno.com>
2017-05-12 15:58:38 -0500

    Added SASS support and refactored global styles into SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp-help')(require('gulp')),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    sass = require('gulp-sass');
 
 gulp.task('connect', 'Starts a server for LiveReload', function() {
   connect.server({
@@ -15,7 +16,14 @@ gulp.task('reload', 'Reloads project files', function() {
 });
 
 gulp.task('watch', 'Watches for changes in project files, calls reload task when changes occur', function() {
+  gulp.watch(['./src/sass/*.s*ss'], ['sass']);
   gulp.watch(['./src/**/*.*'], ['reload']);
 });
 
-gulp.task('default', 'Runs by default', ['connect', 'watch']);
+gulp.task('sass', function() {
+  return gulp.src('./src/sass/*.s*ss')
+             .pipe(sass().on('error', sass.logError))
+             .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('default', 'Runs by default', ['sass', 'connect', 'watch']);
