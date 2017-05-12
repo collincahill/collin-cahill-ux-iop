11ff33ec3de6f2c7b6bf584e3f41046aae4f595f
Collin Cahill <collin.cahill@banno.com>
2017-05-12 14:40:53 -0500

    restructured and refactored HTML pages, added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: './src/',
+    port: 1820,
+    livereload: true
+  });
+});
+
+gulp.task('reload', function() {
+  gulp.src('./src/**/*.*')
+      .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./src/**/*.*'], ['reload']);
+});
+
+gulp.task('default', ['connect', 'watch']);
